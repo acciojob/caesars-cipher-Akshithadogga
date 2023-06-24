4d92868ed16a10a3a8466f9ac1c4aa1141fab56d
Akshithadogga <117561482+Akshithadogga@users.noreply.github.com>
2023-06-24 15:23:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-// Your Script here.
-function rot13(str){
-	const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	const rot13 ="NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	const regex =/[a-zA-Z]/g;
-	const decodedStr =str.replace(regex,(char)=> {
-		const index = rot13.indexOf(char);
-		if(index === -1){
-			return char;
+const lookup = {
+  'A': 'N','B': 'O','C': 'P','D': 'Q',
+  'E': 'R','F': 'S','G': 'T','H': 'U',
+  'I': 'V','J': 'W','K': 'X','L': 'Y',
+  'M': 'Z','N': 'A','O': 'B','P': 'C',
+  'Q': 'D','R': 'E','S': 'F','T': 'G',
+  'U': 'H','V': 'I','W': 'J','X': 'K',
+  'Y': 'L','Z': 'M', '?': '?', ',': ','
+};
+
+function rot13(encodedStr){
+   let decodedArr = []; 
+	for(let i=0; i<encodedStr.length; i++){
+		if(lookup[encodedStr[i]]===undefined){
+			decodedArr.push(encodedStr[i]);
+		}else{
+			decodedArr.push(lookup[encodedStr[i]);
 		}
-		return alphabet[index];
-	});
-	return decodedStr;
+	}
+  return decodedArr;
 }
+console.log(rot13("#AB"));
+
+
 module.exports = rot13;
-
-
